e38e1c4f7c0c365fc7f3d6511c5742864161c69a
Eric Griffis <dedbox@gmail.com>
2017-08-07 01:44:28 -0700

    moved game logic into ball

--- ball.gd
+++ ball.gd
@@ -2,5 +2,51 @@ extends Node2D
 
 export var color = Color(1, 1, 1)
 
+const INITIAL_SPEED = 80
+
+var direction = Vector2(1, 0)
+var speed = INITIAL_SPEED
+var acceleration = 1.1
+var court_size
+var pos
+
+func _ready():
+	court_size = get_viewport_rect().size
+	set_process(true)
+
+func _process(delta):
+	pos = get_pos() + direction * speed * delta
+	try_collide_wall(delta)
+	try_collide_paddles()
+	try_goal()
+	set_pos(pos)
+
 func _draw():
 	draw_circle(Vector2(0, 0), 5, color)
+	
+# flip when touching roof or floor
+func try_collide_wall(delta):
+	if (pos.y < 5 and direction.y < 0) or (pos.y > court_size.height - 5 and direction.y > 0):
+		direction.y = -direction.y
+	
+# flip, change direction, and increase speed when touching paddles
+func try_collide_paddles():
+	try_collide_paddle("left-paddle", -1)
+	try_collide_paddle("right-paddle", 1)
+
+func try_collide_paddle(name, I):
+	var paddle_rect = Rect2(get_parent().get_node(name).get_pos(), Vector2(10, 60))
+	var ball_rect = Rect2(pos - Vector2(5, 5), Vector2(10, 10))
+	if paddle_rect.intersects(ball_rect) and direction.x * I > 0:
+		direction.x = -direction.x
+		direction.y = randf() * 2.0 - 1
+		direction = direction.normalized()
+		speed *= acceleration
+
+# reset after a goal
+func try_goal():
+	if pos.x < 60 or pos.x > court_size.width - 60:
+		pos = court_size * 0.5
+		speed = INITIAL_SPEED
+		# winner serves
+		direction = Vector2(-sign(direction.x), 0)
--- court.gd
+++ court.gd
@@ -3,49 +3,21 @@ extends Node2D
 export var courtColor = Color(0, 0, 0)
 export var goalColor = Color(0.5, 0.5, 0.5)
 
-const INITIAL_SPEED = 80
-
-var size
+var width
+var height
 var aspect
-var direction = Vector2(1, 0)
-var speed = INITIAL_SPEED
-var acceleration = 1.1
 
 func _ready():
-	size = get_viewport_rect().size
-	aspect = size.width / size.height
-	set_process(true)
+	var size = get_viewport_rect().size
+	width = size.width
+	height = size.height
+	aspect = width / height
 	set_process_input(true)
 
-func _process(delta):
-	var ball = get_node("ball")
-	var pos = ball.get_pos()
-	# flip when touching roof or floor
-	pos += direction * speed * delta
-	if (pos.y < 5 and direction.y < 0) or (pos.y > size.height - 5 and direction.y > 0):
-		direction.y = -direction.y
-	# flip, change direction, and increase speed when touching paddles
-	var left_rect = Rect2(get_node("paddle/left").get_pos(), Vector2(10, 60))
-	var right_rect = Rect2(get_node("paddle/right").get_pos(), Vector2(10, 60))
-	var ball_rect = Rect2(pos - Vector2(5, 5), Vector2(10, 10))
-	if (left_rect.intersects(ball_rect) and direction.x < 0) or (right_rect.intersects(ball_rect) and direction.x > 0):
-		direction.x = -direction.x
-		direction.y = randf() * 2.0 - 1
-		direction = direction.normalized()
-		speed *= acceleration
-	# check game over
-	if pos.x < 60 or pos.x > size.width - 60:
-		pos = size * 0.5
-		speed = INITIAL_SPEED
-		direction = Vector2(-sign(direction.x), 0)
-	ball.set_pos(pos)
-
 func _draw():
-	var W = size.width - 1
-	var H = size.height - 1
-	draw_rect(Rect2(1, 1, size.width, size.height), courtColor)
-	draw_rect(Rect2(0, 0, 60, size.height), goalColor)
-	draw_rect(Rect2(size.width - 60, 0, size.width, size.height), goalColor)
+	draw_rect(Rect2(1, 1, width, height), courtColor)
+	draw_rect(Rect2(0, 0, 60, height), goalColor)
+	draw_rect(Rect2(width - 60, 0, width, height), goalColor)
 
 func _input(ev):
 	if ev.type == InputEvent.MOUSE_MOTION:
@@ -53,15 +25,15 @@ func _input(ev):
 		move_right_paddle_to(ev.pos.x)
 
 func move_left_paddle_to(y):
-	var pad = get_node("paddle/left")
+	var pad = get_node("left-paddle")
 	var pos = pad.get_pos()
-	var ofs = max(0, 60 * min(y, size.height) / size.height)
-	pos.y = min(max(y, 0), size.height) - ofs
+	var ofs = max(0, 60 * min(y, height) / height)
+	pos.y = min(max(y, 0), height) - ofs
 	pad.set_pos(pos)
 
 func move_right_paddle_to(x):
-	var pad = get_node("paddle/right")
+	var pad = get_node("right-paddle")
 	var pos = pad.get_pos()
-	var ofs = max(0, 60 * min(x, size.width) / size.width)
-	pos.y = min(max(x, 0), size.width) / aspect - ofs
+	var ofs = max(0, 60 * min(x, width) / width)
+	pos.y = min(max(x, 0), width) / aspect - ofs
 	pad.set_pos(pos)
