32b685bb10559fe0dbaddb0bf4d4660f8c68b2fb
Eric Griffis <dedbox@gmail.com>
2017-08-07 01:54:41 -0700

    move input logic into paddle

--- court.gd
+++ court.gd
@@ -21,19 +21,5 @@ func _draw():
 
 func _input(ev):
 	if ev.type == InputEvent.MOUSE_MOTION:
-		move_left_paddle_to(ev.pos.y)
-		move_right_paddle_to(ev.pos.x)
-
-func move_left_paddle_to(y):
-	var pad = get_node("left-paddle")
-	var pos = pad.get_pos()
-	var ofs = max(0, 60 * min(y, height) / height)
-	pos.y = min(max(y, 0), height) - ofs
-	pad.set_pos(pos)
-
-func move_right_paddle_to(x):
-	var pad = get_node("right-paddle")
-	var pos = pad.get_pos()
-	var ofs = max(0, 60 * min(x, width) / width)
-	pos.y = min(max(x, 0), width) / aspect - ofs
-	pad.set_pos(pos)
+		get_node("left-paddle").move_to(ev.pos.y, height)
+		get_node("right-paddle").move_to(ev.pos.x, width, aspect)
--- paddle.gd
+++ paddle.gd
@@ -5,3 +5,9 @@ export var color = Color(1, 1, 1)
 func _draw():
 	var area = Rect2(0, 0, 10, 60)
 	draw_rect(area, color)
+
+func move_to(c, c_max, aspect=1):
+	var pos = get_pos()
+	var offset = max(0, 60 * min(c, c_max) / c_max)
+	pos.y = min(max(c, 0), c_max) / aspect - offset
+	set_pos(pos)
